147423a1643d4f0cc8524b2683f159876e1ba3e2
Melcin Herbie Choquecondo Quispe <mchoquecondoq@unsa.edu.pe>
2026-05-28 21:43:22 -0500

    Realize la funcion fractal_tree que genera un arbol binario como lo indica la actividad 1 de la practica ejecute el archivo con make para observar su comportamiento grafico

--- examples/testLine.c
+++ examples/testLine.c
@@ -1,7 +1,24 @@
 #include "../turtlec.h"
+void fractal_tree(Turtle *t, float length , int depth){
+    if(depth == 0){
+        return;
+    }
+    
+    turtleForward(t , length);
+    
+    turtleRight(t, 30.0);
+    fractal_tree(t, length*0.7, depth -1);
+    turtleLeft(t, 60.0);
+    fractal_tree(t, length*0.7, depth -1);
+    turtleRight(t, 30.0);
+    turtleBackward(t, length);
+}
+
+
+
 
 int main(void){
-  TurtleApp *app = turtleAppCreate(400, 200, "Test Line");
+  TurtleApp *app = turtleAppCreate(900, 900, "Test Line");
 
   if(app == NULL)
     return 1;
@@ -9,13 +26,12 @@ int main(void){
   Turtle *t = turtleAppGetTurtle(app);
 
   turtlePenUp(t);
-  turtleGoTo(t, 50.0f, 100.0f);
+  turtleGoTo(t, 0.0f,400.0f);
   turtlePenDown(t);
 
   turtleSetColor(t, 255, 100, 0);
   turtleSetSpeed(t, 5.0f);
-  turtleForward(t, 300.0f);
-
+  fractal_tree(t, 200, 4);
   turtleAppRun(app);
   turtleAppDestroy(app);
   return 0;
