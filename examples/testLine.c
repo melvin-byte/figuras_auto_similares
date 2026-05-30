a3c3c6ee7b6a6b4b5b4dc4c3c7a68b35d1b46e38
Melcin Herbie Choquecondo Quispe <mchoquecondoq@unsa.edu.pe>
2026-05-30 14:55:21 -0500

    No estoy seguro de que sea la respuesta pero realize el punto 2 del reto modifcando el tama;o de la rama derecha e izquierda a valores distintos en la funcion fractal_tree que tenia hecha asigne 0.5 y 0.7 en los valores de la fucnion recursiva para generar un arbol con ramas de tama;os distintos.

--- examples/testLine.c
+++ examples/testLine.c
@@ -15,7 +15,7 @@ void fractal_tree(Turtle *t, float length , int depth){
     turtleRight(t, 20.0);
     fractal_tree(t, length*0.5, depth -1);
     turtleLeft(t, 40.0);
-    fractal_tree(t, length*0.5, depth -1);
+    fractal_tree(t, length*0.7, depth -1);
     turtleRight(t, 20.0);
     turtleBackward(t, length);
 }
@@ -66,7 +66,8 @@ int main(void){
 
   turtleSetColor(t, 255, 100, 0);
   turtleSetSpeed(t, 5.0f);
-  fractal_tree_reto_final(t, 200, 5);
+  fractal_tree(t, 200, 5);
+  //fractal_tree_reto_final(t, 200, 5);
   //Levy(t, 200 , 9);
   turtleAppRun(app);
   turtleAppDestroy(app);
