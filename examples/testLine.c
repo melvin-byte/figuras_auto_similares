fded93c986a17816ff341f6918bdc4f5e8244cf8
Melcin Herbie Choquecondo Quispe <mchoquecondoq@unsa.edu.pe>
2026-05-29 20:38:00 -0500

    edicion de la actividad 2 se realizo camvios en los parametros de tama;o, profundidad, y se agrego una condicional para verificar graficamente el color que toma el fractal cruzando cierta profundidad.

--- examples/testLine.c
+++ examples/testLine.c
@@ -3,14 +3,19 @@ void fractal_tree(Turtle *t, float length , int depth){
     if(depth == 0){
         return;
     }
+    if (depth > 5){
+        turtleSetColor(t, 120 , 70 , 20);
+    }else{
+        turtleSetColor(t, 0 , 200 , 0);
+    }
     
     turtleForward(t , length);
     
-    turtleRight(t, 30.0);
-    fractal_tree(t, length*0.7, depth -1);
-    turtleLeft(t, 60.0);
-    fractal_tree(t, length*0.7, depth -1);
-    turtleRight(t, 30.0);
+    turtleRight(t, 20.0);
+    fractal_tree(t, length*0.5, depth -1);
+    turtleLeft(t, 40.0);
+    fractal_tree(t, length*0.5, depth -1);
+    turtleRight(t, 20.0);
     turtleBackward(t, length);
 }
 
@@ -31,7 +36,7 @@ int main(void){
 
   turtleSetColor(t, 255, 100, 0);
   turtleSetSpeed(t, 5.0f);
-  fractal_tree(t, 200, 4);
+  fractal_tree(t, 200, 7);
   turtleAppRun(app);
   turtleAppDestroy(app);
   return 0;
